1d2678a07d8ee627e5a65dca2afb767188a3824d
Alex Bradatan <37741488+BreadyX@users.noreply.github.com>
2018-11-08 16:31:05 +0100

    fixed typos

--- postinstall.sh
+++ postinstall.sh
@@ -4,11 +4,12 @@ cat packages.txt | xargs pacman -S
 
 systemctl enable bluetooth.service
 systemctl enable acpid.service
-systemctl eamble fstrim.timer
+systemctl enable fstrim.timer
+systemctl enable NetworkManager
 
 echo "exec startkde" > ~/.xinitrc
 systemctl enable sddm
 
 useradd -m -g users -G wheel -s /bin/bash breadyx
 passwd breadyx
-EDITOR=nani visudo
+EDITOR=nano visudo
