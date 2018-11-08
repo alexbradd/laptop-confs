469c66f00b2b4ea5d86684a8aa044c0681657c9a
Alex Bradatan <37741488+BreadyX@users.noreply.github.com>
2018-11-08 16:09:30 +0100

    fixed some errors in install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,13 @@ echo "KEYMAP=it" > /etc/vconsole.conf
 echo "breadyx-laptop" > /etc/hostname
 echo "127.0.0.1	localhost" > /etc/hosts
 echo "::1 localhost" >> /etc/hosts
-echo "127.0.1.1 breadyx-laptop.localdomain breadyx-laptop"
+echo "127.0.1.1 breadyx-laptop.localdomain breadyx-laptop" >> /etc/hosts
 
 mkinitcpio -p linux
 passwd 
 
 pacman -S grub efibootmgr
-mkdir /boot/efi
-mount /dev/sda1 /boot/efi
+# mkdir /boot/efi
+# mount /dev/sda1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=ARCH --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
