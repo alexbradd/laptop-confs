01fcfb05db18bf599aaffe0d381a140ff3ca2fa9
Alex Bradatan <37741488+BreadyX@users.noreply.github.com>
2018-11-30 14:16:45 +0100

    added localectl to post_chroot.sh

--- post_chroot.sh
+++ post_chroot.sh
@@ -11,6 +11,7 @@ sleep 2s; nano /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=it" > /etc/vconsole.conf
+localectl set-keymap it
 
 #HOST
 echo "breadyx-laptop" > /etc/hostname
