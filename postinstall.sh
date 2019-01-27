8bb24210098934f07e7532cdd6963dda54145bff
BreadyX <alex.bradatan85@yahoo.it>
2019-01-27 15:38:16 +0100

    Added copying of root folder to / into postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,8 @@
 
 xargs pacman -S --noconfirm < packages.txt
 
+cp -rf root/* /
+
 systemctl enable bluetooth.service
 systemctl enable acpid.service
 systemctl enable fstrim.timer
@@ -11,3 +13,6 @@ systemctl enable tlp.service
 systemctl enable tlp-sleep.service
 systemctl mask systemd-rfkill.service
 systemctl mask systemd-rfkill.socket
+
+systemctl enable rc.service
+systemctl enable undervolt.service
